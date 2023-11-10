39a9c7a33cbaccf43ae88eb7f22de587d28d2856
Fabián Karaben <fabiankaraben@gmail.com>
2023-11-10 12:46:29 -0300

    Rearranges the Python course

--- next.config.js
+++ next.config.js
@@ -40,6 +40,91 @@ module.exports = withNextra({
         destination: '/',
         permanent: true,
       },
+      {
+        source: '/python/conceptos/tipos-de-datos',
+        destination: '/python/tipos-de-datos',
+        permanent: true,
+      },
+      {
+        source: '/python/conceptos/variables',
+        destination: '/python/variables',
+        permanent: true,
+      },
+      {
+        source: '/python/conceptos/expresiones-logicas',
+        destination: '/python/expresiones-logicas',
+        permanent: true,
+      },
+      {
+        source: '/python/conceptos/funciones-y-metodos',
+        destination: '/python/funciones-y-metodos',
+        permanent: true,
+      },
+      {
+        source: '/python/conceptos/otros',
+        destination: '/python/conceptos',
+        permanent: true,
+      },
+      {
+        source: '/python/primer-programa/tipeado-compilacion-testing',
+        destination: '/python/primer-programa',
+        permanent: true,
+      },
+      {
+        source: '/python/primer-programa/estructura-basica',
+        destination: '/python/primer-programa',
+        permanent: true,
+      },
+      {
+        source: '/python/primer-programa/primer-programa',
+        destination: '/python/primer-programa',
+        permanent: true,
+      },
+      {
+        source: '/python/primer-programa/comentarios',
+        destination: '/python/primer-programa',
+        permanent: true,
+      },
+      {
+        source: '/python/primer-programa/caso-de-error',
+        destination: '/python/primer-programa',
+        permanent: true,
+      },
+      {
+        source: '/python/estructuras-de-control',
+        destination: '/python/condicionales',
+        permanent: true,
+      },
+      {
+        source: '/python/estructuras-de-control/condicionales',
+        destination: '/python/condicionales',
+        permanent: true,
+      },
+      {
+        source: '/python/estructuras-de-control/bucles',
+        destination: '/python/bucles',
+        permanent: true,
+      },
+      {
+        source: '/python/estructuras-de-control/expresiones-asignacion',
+        destination: '/python/expresiones-asignacion',
+        permanent: true,
+      },
+      {
+        source: '/python/archivos/leer-archivo',
+        destination: '/python/archivos',
+        permanent: true,
+      },
+      {
+        source: '/python/archivos/escribir-archivo',
+        destination: '/python/archivos',
+        permanent: true,
+      },
+      {
+        source: '/python/archivos/generar-archivo',
+        destination: '/python/archivos',
+        permanent: true,
+      },
     ];
   },
 })
